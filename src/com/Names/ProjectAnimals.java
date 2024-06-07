63eab4759d6f41ae0083f04d280d5b861498c76c
lakshmiqa1 <billaluckyrock@gmail.com>
2024-06-07 15:18:04 +0530

    added three methods

---
+++ src/com/Names/ProjectAnimals.java
@@ -0,0 +1,17 @@
+package com.Names;
+
+public class ProjectAnimals {
+	private void tiger() {
+		System.out.println("national animal");
+
+	}
+	private void elephant() {
+		System.out.println("biggest animal");
+
+	}
+	private void camel() {
+		System.out.println("longest neck");
+
+	}
+
+}
